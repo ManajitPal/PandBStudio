a00c8e581c2fa197b47e3d9efdab68b41491a6b6
Manajit Pal <hello@manajitpal.online>
2018-09-27 02:37:07 +0530

    Feat: Redesign using parallax and scroll snap

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,59 @@
+/**
+ * Load all the functions once DOM tree is loaded.
+ */
 document.addEventListener('DOMContentLoaded', (event) => {
 
-// 	window.addEventListener("scroll", function () {
-    
-// });
-// 		let scrollDiv = document.getElementById('scroll');
-// 		scrollDiv.addEventListener('scroll', (event) => {
-// 			let scrollDivStyle = scrollDiv.currentStyle || window.getComputedStyle(scrollDiv);
-// 			console.log("current Margin: ", scrollDivStyle);
-// 		});
-    let lazyImages = [].slice.call(document.querySelectorAll('img.lazyLoad'));
+	/**
+	 * Dynamically add margin Bottom to main from Footer height.
+	 */
+	const footer = document.getElementsByTagName('footer');
+	let main = document.getElementsByTagName('main');
+	main[0].style.marginBottom = getComputedStyle(footer[0]).height; 
+
+	/**
+	 * Dynamically add project list.
+	 */
+	const projectNames = ['1', '2', '3'];
+	const ul = document.getElementById('project-container');
+	projectNames.forEach((projectName) => {
+		ul.append(createProjectList(projectName));
+	});
+
+	/**
+	 * Calling LazyLoad and Polyfill
+	 */
+	LazyLoadImages();
+	cssScrollSnapPolyfill();
+
+});
+
+createProjectList = (projectName) => {
+
+	const li = document.createElement('li');
+
+	const a = document.createElement('a');
+	a.href = `https://google.com/${projectName}`;
+	a.setAttribute('target','_blank');
+	a.setAttribute('aria-label',`Link to project ${projectName}`);
+
+	const img = document.createElement('img');
+	img.className = 'lazyLoad';
+	img.alt = `Image for project ${projectName}`;
+	img.src = 'data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+	img.srcset = 'data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+	img.setAttribute('data-src',`img/Project${projectName}/${projectName}.webp`);
+	img.setAttribute('data-srcset',`img/Project${projectName}/${projectName}@2x.webp 2x, img/Project${projectName}/${projectName}@3x.webp 3x`);
+
+	a.appendChild(img);
+	li.append(a);
+
+	return li;
+
+};
+
+LazyLoadImages = () => {
+
+	let lazyImages = [].slice.call(document.querySelectorAll('img.lazyLoad'));
 	if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
 	  let lazyImageObserver = new IntersectionObserver((entries, observer) => {
 		entries.forEach((entry) => {
@@ -27,4 +72,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
 		lazyImageObserver.observe(lazyImage);
 	  });
 	}
-})
+
+}
